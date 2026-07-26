020c732841ac11328b1d67ade2e417727ff139f2
Khaled Mohamed <km00550055@gmail.com>
2026-07-26 00:06:40 -0700

    Application Types Business & DataAccess Is Done

---
+++ DVLD_Business/clApplicationType.cs
@@ -0,0 +1,53 @@
+﻿using DVLD_DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Business
+{
+    public class clApplicationType
+    {
+        public int AppTypeID { get; set; }
+        public string AppTypeTitle { get; set; }
+        public float AppTypeFees { get; set; }
+
+        public clApplicationType()
+        {
+            this.TypeID = -1;
+            this.Title = string.Empty;
+            this.Fees = 0f;
+        }
+        private clApplicationType(int TypeID, string Title, float Fees)
+        {
+            this.TypeID = TypeID;
+            this.Title = Title;
+            this.Fees = Fees;
+        }
+
+        static public DataTable GetAllAppTypes()
+        {
+            return clApplicationTypeData.LoadAllAppTypes();
+        }
+
+        static public clApplicationType Find(int ApplicationTypeID)
+        {
+            string Title = string.Empty;
+            float Fees = 0f;
+
+            if (clApplicationTypeData.LoadAppType(ApplicationTypeID, ref Title, ref Fees))
+            {
+                return new clApplicationType(ApplicationTypeID, Title, Fees);
+            }
+
+            else return null;
+        }
+        
+        public bool UpdateTypeInfo(string NewTitle = this.AppTypeTitle, float NewFees = this.AppTypeFees)
+        {
+            return clApplicationTypeData.UpdateAppType(this.TypeID, NewTitle, NewFees);
+        }
+    }
+}
---
+++ DVLD_DataAccess/clApplicationTypeData.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess
+{
+    static public class clApplicationTypeData
+    {
+        static public DataTable LoadAllAppTypes()
+        {
+            string Query = @"SELECT * FROM ApplicationTypes";
+
+            DataTable dtUsersList = new DataTable();
+
+            using (SqlConnection Connection = new SqlConnection(DVLD_DataAccess.clSettings.ConnectionString))
+            {
+                using (SqlCommand Command = new SqlCommand(Query, Connection))
+                {
+                    Connection.Open();
+
+                    SqlDataReader Reader = Command.ExecuteReader();
+
+                    dtUsersList.Load(Reader);
+                }
+            }
+
+            return dtUsersList;
+        }
+        static public bool LoadAppType(int ApplicationTypeID, ref string Title, ref float Fees)
+        {
+            bool IsFound = false;
+
+            string Query = @"SELECT * FROM ApplicationTypes WHERE ApplicationTypeID = @ApplicationTypeID";
+
+            using (SqlConnection connection = new SqlConnection(DVLD_DataAccess.clSettings.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(Query, connection))
+                {
+                    command.Parameters.AddWithValue("@ApplicationTypeID", ApplicationTypeID);
+
+                    connection.Open();
+
+                    using (SqlDataReader Reader = command.ExecuteReader())
+                    {
+                        if (Reader.Read())
+                        {
+                            Title = Reader["ApplicationTypeTitle"].ToString();
+                            Fees = Convert.ToSingle(Reader["ApplicationFees"]);
+
+                            IsFound = true;
+                        }
+                    }
+                }
+            }
+
+            return IsFound;
+        }
+        static public bool UpdateAppType(int TypeID, string NewTitle, float NewFees)
+        {
+            string Query = @"Update ApplicationTypes 
+                            Set ApplicationTypeTitle = @NewTitle, ApplicationFees = @NewFees
+                             WHERE ApplicationTypeID = @TypeID;";
+
+            int RowsAffected = 0;
+
+            using (SqlConnection Connection = new SqlConnection(DVLD_DataAccess.clSettings.ConnectionString))
+            using (SqlCommand Command = new SqlCommand(Query, Connection))
+            {
+                Command.Parameters.AddWithValue("@TypeID", TypeID);
+                Command.Parameters.AddWithValue("@NewTitle", NewTitle);
+                Command.Parameters.AddWithValue("@NewFees", NewFees);
+
+                Connection.Open();
+                RowsAffected = Convert.ToInt32(Command.ExecuteNonQuery());
+
+            }
+
+            return RowsAffected > 0;
+        }
+    }
+}
