72f94f1a4c753db118831dfa4a99d1afdc645aad
Khaled Mohamed <km00550055@gmail.com>
2026-07-26 07:50:41 -0700

    Test Types Business & DataAccess Is Done

---
+++ DVLD_Business/clTestType.cs
@@ -0,0 +1,57 @@
+﻿using DVLD_DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Business
+{
+    public class clTestType
+    {
+        public enum eTestType { Vision = 1, Written, Street}
+        public eTestType ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public float Fees { get; set; }
+
+        public clTestType()
+        {
+            this.ID = eTestType.Vision;
+            this.Title = string.Empty;
+            this.Description = string.Empty;
+            this.Fees = 0f;
+        }
+        private clTestType(eTestType ID, string Title, string Description, float Fees)
+        {
+            this.ID = ID;
+            this.Title = Title;
+            this.Description = Description;
+            this.Fees = Fees;
+        }
+
+        static public DataTable GetAllTestTypes()
+        {
+            return clTestTypeData.LoadAllTestTypes();
+        }
+        static public clTestType Find(byte ID)
+        {
+            string Title = string.Empty;
+            string Description = string.Empty;
+            float  Fees = 0f;
+
+            if (clTestTypeData.LoadTestType(ID, ref Title, ref Description, ref Fees))
+            {
+                return new clTestType((eTestType)ID, Title, Description, Fees);
+            }
+
+            else return null;
+        }
+
+        public bool UpdateTestInfo()
+        {
+            return clTestTypeData.UpdateTestType((byte)this.ID, this.Title, this.Description, this.Fees);
+        }
+    }
+}
