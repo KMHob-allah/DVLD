725a2965bea1eff3f0f3fc668d570d26b2b426a8
Khaled Mohamed <km00550055@gmail.com>
2026-07-31 23:45:06 -0700

    Schduled Test Form UI Is Done

---
+++ DVLD/Tests/TestAppointments/frmSchduledTest.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace DVLD.Tests.TestAppointments
+{
+    partial class frmSchduledTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTestIDValue = new System.Windows.Forms.Label();
+            this.lblTestID = new System.Windows.Forms.Label();
+            this.rdbtnFail = new System.Windows.Forms.RadioButton();
+            this.rdbtnPass = new System.Windows.Forms.RadioButton();
+            this.tbNotesValue = new System.Windows.Forms.TextBox();
+            this.lblNotes = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblDateValue = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblLocalAppIDValue = new System.Windows.Forms.Label();
+            this.lblDrivingClassValue = new System.Windows.Forms.Label();
+            this.lblNameValue = new System.Windows.Forms.Label();
+            this.lblTrailValue = new System.Windows.Forms.Label();
+            this.lblFeesValue = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblTrail = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblFees = new System.Windows.Forms.Label();
+            this.lblDrivingClass = new System.Windows.Forms.Label();
+            this.lblDrivingLicenseAppID = new System.Windows.Forms.Label();
+            this.lblSchduledTest = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTestIDValue
+            // 
+            this.lblTestIDValue.AutoSize = true;
+            this.lblTestIDValue.Font = new System.Drawing.Font("Bookman Old Style", 10F, System.Drawing.FontStyle.Bold);
+            this.lblTestIDValue.Location = new System.Drawing.Point(276, 406);
+            this.lblTestIDValue.Name = "lblTestIDValue";
+            this.lblTestIDValue.Size = new System.Drawing.Size(136, 20);
+            this.lblTestIDValue.TabIndex = 81;
+            this.lblTestIDValue.Text = "Not Taken Yet";
+            // 
+            // lblTestID
+            // 
+            this.lblTestID.AutoSize = true;
+            this.lblTestID.Font = new System.Drawing.Font("Bookman Old Style", 10F, System.Drawing.FontStyle.Bold);
+            this.lblTestID.Location = new System.Drawing.Point(48, 406);
+            this.lblTestID.Name = "lblTestID";
+            this.lblTestID.Size = new System.Drawing.Size(74, 20);
+            this.lblTestID.TabIndex = 80;
+            this.lblTestID.Text = "Test ID";
+            // 
+            // rdbtnFail
+            // 
+            this.rdbtnFail.AutoSize = true;
+            this.rdbtnFail.Font = new System.Drawing.Font("Bookman Old Style", 10F);
+            this.rdbtnFail.Location = new System.Drawing.Point(264, 494);
+            this.rdbtnFail.Name = "rdbtnFail";
+            this.rdbtnFail.Size = new System.Drawing.Size(62, 25);
+            this.rdbtnFail.TabIndex = 79;
+            this.rdbtnFail.TabStop = true;
+            this.rdbtnFail.Text = "Fail";
+            this.rdbtnFail.UseVisualStyleBackColor = true;
+            // 
+            // rdbtnPass
+            // 
+            this.rdbtnPass.AutoSize = true;
+            this.rdbtnPass.Font = new System.Drawing.Font("Bookman Old Style", 10F);
+            this.rdbtnPass.Location = new System.Drawing.Point(143, 494);
+            this.rdbtnPass.Name = "rdbtnPass";
+            this.rdbtnPass.Size = new System.Drawing.Size(70, 25);
+            this.rdbtnPass.TabIndex = 78;
+            this.rdbtnPass.TabStop = true;
+            this.rdbtnPass.Text = "Pass";
+            this.rdbtnPass.UseVisualStyleBackColor = true;
+            // 
+            // tbNotesValue
+            // 
+            this.tbNotesValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbNotesValue.Location = new System.Drawing.Point(143, 543);
+            this.tbNotesValue.Multiline = true;
+            this.tbNotesValue.Name = "tbNotesValue";
+            this.tbNotesValue.Size = new System.Drawing.Size(388, 93);
+            this.tbNotesValue.TabIndex = 77;
+            // 
+            // lblNotes
+            // 
+            this.lblNotes.AutoSize = true;
+            this.lblNotes.Font = new System.Drawing.Font("Bookman Old Style", 10F, System.Drawing.FontStyle.Bold);
+            this.lblNotes.Location = new System.Drawing.Point(48, 542);
+            this.lblNotes.Name = "lblNotes";
+            this.lblNotes.Size = new System.Drawing.Size(59, 20);
+            this.lblNotes.TabIndex = 76;
+            this.lblNotes.Text = "Notes";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Bookman Old Style", 10F, System.Drawing.FontStyle.Bold);
+            this.lblResult.Location = new System.Drawing.Point(48, 494);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(66, 20);
+            this.lblResult.TabIndex = 75;
+            this.lblResult.Text = "Result";
+            // 
+            // lblDateValue
+            // 
+            this.lblDateValue.AutoSize = true;
+            this.lblDateValue.Font = new System.Drawing.Font("Bookman Old Style", 10F);
+            this.lblDateValue.Location = new System.Drawing.Point(276, 357);
+            this.lblDateValue.Name = "lblDateValue";
+            this.lblDateValue.Size = new System.Drawing.Size(37, 21);
+            this.lblDateValue.TabIndex = 74;
+            this.lblDateValue.Text = "???";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Bookman Old Style", 12F);
+            this.btnClose.Image = global::DVLD.Properties.Resources.Close_32;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.Location = new System.Drawing.Point(143, 680);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(134, 45);
+            this.btnClose.TabIndex = 72;
+            this.btnClose.Text = "Close";
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Bookman Old Style", 12F);
+            this.btnSave.Image = global::DVLD.Properties.Resources.Save_32;
+            this.btnSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSave.Location = new System.Drawing.Point(397, 680);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(134, 45);
+            this.btnSave.TabIndex = 73;
+            this.btnSave.Text = "Save";
+            this.btnSave.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // lblLocalAppIDValue
+            // 
+            this.lblLocalAppIDValue.AutoSize = true;
+            this.lblLocalAppIDValue.Font = new System.Drawing.Font("Bookman Old Style", 10F);
+            this.lblLocalAppIDValue.Location = new System.Drawing.Point(276, 122);
+            this.lblLocalAppIDValue.Name = "lblLocalAppIDValue";
+            this.lblLocalAppIDValue.Size = new System.Drawing.Size(45, 21);
+            this.lblLocalAppIDValue.TabIndex = 71;
+            this.lblLocalAppIDValue.Text = "N/A";
+            // 
+            // lblDrivingClassValue
+            // 
+            this.lblDrivingClassValue.AutoSize = true;
+            this.lblDrivingClassValue.Font = new System.Drawing.Font("Bookman Old Style", 10F);
+            this.lblDrivingClassValue.Location = new System.Drawing.Point(276, 169);
+            this.lblDrivingClassValue.Name = "lblDrivingClassValue";
+            this.lblDrivingClassValue.Size = new System.Drawing.Size(37, 21);
+            this.lblDrivingClassValue.TabIndex = 70;
+            this.lblDrivingClassValue.Text = "???";
+            // 
+            // lblNameValue
+            // 
+            this.lblNameValue.AutoSize = true;
+            this.lblNameValue.Font = new System.Drawing.Font("Bookman Old Style", 10F);
+            this.lblNameValue.Location = new System.Drawing.Point(276, 216);
+            this.lblNameValue.Name = "lblNameValue";
+            this.lblNameValue.Size = new System.Drawing.Size(37, 21);
+            this.lblNameValue.TabIndex = 69;
+            this.lblNameValue.Text = "???";
+            // 
+            // lblTrailValue
+            // 
+            this.lblTrailValue.AutoSize = true;
+            this.lblTrailValue.Font = new System.Drawing.Font("Bookman Old Style", 10F);
+            this.lblTrailValue.Location = new System.Drawing.Point(276, 263);
+            this.lblTrailValue.Name = "lblTrailValue";
+            this.lblTrailValue.Size = new System.Drawing.Size(20, 21);
+            this.lblTrailValue.TabIndex = 68;
+            this.lblTrailValue.Text = "0";
+            // 
+            // lblFeesValue
+            // 
+            this.lblFeesValue.AutoSize = true;
+            this.lblFeesValue.Font = new System.Drawing.Font("Bookman Old Style", 10F);
+            this.lblFeesValue.Location = new System.Drawing.Point(276, 310);
+            this.lblFeesValue.Name = "lblFeesValue";
+            this.lblFeesValue.Size = new System.Drawing.Size(20, 21);
+            this.lblFeesValue.TabIndex = 67;
+            this.lblFeesValue.Text = "0";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Bookman Old Style", 10F, System.Drawing.FontStyle.Bold);
+            this.lblName.Location = new System.Drawing.Point(48, 218);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(59, 20);
+            this.lblName.TabIndex = 66;
+            this.lblName.Text = "Name";
+            // 
+            // lblTrail
+            // 
+            this.lblTrail.AutoSize = true;
+            this.lblTrail.Font = new System.Drawing.Font("Bookman Old Style", 10F, System.Drawing.FontStyle.Bold);
+            this.lblTrail.Location = new System.Drawing.Point(48, 266);
+            this.lblTrail.Name = "lblTrail";
+            this.lblTrail.Size = new System.Drawing.Size(51, 20);
+            this.lblTrail.TabIndex = 65;
+            this.lblTrail.Text = "Trail";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Bookman Old Style", 10F, System.Drawing.FontStyle.Bold);
+            this.lblDate.Location = new System.Drawing.Point(48, 362);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(50, 20);
+            this.lblDate.TabIndex = 64;
+            this.lblDate.Text = "Date";
+            // 
+            // lblFees
+            // 
+            this.lblFees.AutoSize = true;
+            this.lblFees.Font = new System.Drawing.Font("Bookman Old Style", 10F, System.Drawing.FontStyle.Bold);
+            this.lblFees.Location = new System.Drawing.Point(48, 314);
+            this.lblFees.Name = "lblFees";
+            this.lblFees.Size = new System.Drawing.Size(50, 20);
+            this.lblFees.TabIndex = 63;
+            this.lblFees.Text = "Fees";
+            // 
+            // lblDrivingClass
+            // 
+            this.lblDrivingClass.AutoSize = true;
+            this.lblDrivingClass.Font = new System.Drawing.Font("Bookman Old Style", 10F, System.Drawing.FontStyle.Bold);
+            this.lblDrivingClass.Location = new System.Drawing.Point(48, 170);
+            this.lblDrivingClass.Name = "lblDrivingClass";
+            this.lblDrivingClass.Size = new System.Drawing.Size(126, 20);
+            this.lblDrivingClass.TabIndex = 62;
+            this.lblDrivingClass.Text = "Driving Class";
+            // 
+            // lblDrivingLicenseAppID
+            // 
+            this.lblDrivingLicenseAppID.AutoSize = true;
+            this.lblDrivingLicenseAppID.Font = new System.Drawing.Font("Bookman Old Style", 10F, System.Drawing.FontStyle.Bold);
+            this.lblDrivingLicenseAppID.Location = new System.Drawing.Point(48, 122);
+            this.lblDrivingLicenseAppID.Name = "lblDrivingLicenseAppID";
+            this.lblDrivingLicenseAppID.Size = new System.Drawing.Size(212, 20);
+            this.lblDrivingLicenseAppID.TabIndex = 61;
+            this.lblDrivingLicenseAppID.Text = "Driving License App ID";
+            // 
+            // lblSchduledTest
+            // 
+            this.lblSchduledTest.AutoSize = true;
+            this.lblSchduledTest.Font = new System.Drawing.Font("Bookman Old Style", 20F, System.Drawing.FontStyle.Bold);
+            this.lblSchduledTest.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblSchduledTest.Location = new System.Drawing.Point(155, 38);
+            this.lblSchduledTest.Name = "lblSchduledTest";
+            this.lblSchduledTest.Size = new System.Drawing.Size(270, 40);
+            this.lblSchduledTest.TabIndex = 60;
+            this.lblSchduledTest.Text = "Schduled Test";
+            // 
+            // frmSchduledTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(589, 747);
+            this.Controls.Add(this.lblTestIDValue);
+            this.Controls.Add(this.lblTestID);
+            this.Controls.Add(this.rdbtnFail);
+            this.Controls.Add(this.rdbtnPass);
+            this.Controls.Add(this.tbNotesValue);
+            this.Controls.Add(this.lblNotes);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblDateValue);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblLocalAppIDValue);
+            this.Controls.Add(this.lblDrivingClassValue);
+            this.Controls.Add(this.lblNameValue);
+            this.Controls.Add(this.lblTrailValue);
+            this.Controls.Add(this.lblFeesValue);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblTrail);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblFees);
+            this.Controls.Add(this.lblDrivingClass);
+            this.Controls.Add(this.lblDrivingLicenseAppID);
+            this.Controls.Add(this.lblSchduledTest);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmSchduledTest";
+            this.Text = "`";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTestIDValue;
+        private System.Windows.Forms.Label lblTestID;
+        private System.Windows.Forms.RadioButton rdbtnFail;
+        private System.Windows.Forms.RadioButton rdbtnPass;
+        private System.Windows.Forms.TextBox tbNotesValue;
+        private System.Windows.Forms.Label lblNotes;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblDateValue;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblLocalAppIDValue;
+        private System.Windows.Forms.Label lblDrivingClassValue;
+        private System.Windows.Forms.Label lblNameValue;
+        private System.Windows.Forms.Label lblTrailValue;
+        private System.Windows.Forms.Label lblFeesValue;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblTrail;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblFees;
+        private System.Windows.Forms.Label lblDrivingClass;
+        private System.Windows.Forms.Label lblDrivingLicenseAppID;
+        private System.Windows.Forms.Label lblSchduledTest;
+    }
+}
