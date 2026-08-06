c14b5f6b28cffbbcda46823d567ba4a7d146440a
Khaled Mohamed <km00550055@gmail.com>
2026-08-05 22:39:33 -0700

    Driver Data & Business Done

---
+++ DVLD_Business/clDriver.cs
@@ -0,0 +1,131 @@
+﻿using DVLD_DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Business
+{
+    public class clDriver
+    {
+        public enum eMode { Add, Update}
+
+        public int ID { get; set; }
+        public int PersonID { get; set; }
+        public int CreatedByUserID { get; set; }
+        public DateTime CreationDate { get;}
+
+        public clPerson PersonInfo { get; set; }
+        public clUser UserInfo {  get; set; }
+
+        eMode _Mode;
+
+
+        public clDriver()
+        {
+            this.ID = -1;
+            this.PersonID = -1;
+            this.CreatedByUserID = -1;
+            this.CreationDate = DateTime.MinValue;
+
+            _Mode = eMode.Add;
+        }
+        private clDriver(int ID, int PersonID, int CreatedByUserID, DateTime CreationDate)
+        {
+            this.ID = ID;
+            this.PersonID = PersonID;
+            this.CreatedByUserID = CreatedByUserID;
+            this.CreationDate = CreationDate;
+
+            _Mode = eMode.Update;
+
+            this.PersonInfo = clPerson.Find(PersonID);
+            this.UserInfo = clUser.FindByUserID(CreatedByUserID);
+        }
+
+        static public DataTable GetAllDrivers()
+        {
+            return clDriverData.LoadDriversList();
+        }
+
+        static public DataTable GetLicenses(int DriverID)
+        {
+            return clLicenseData.LoadDriverLicenses();
+        }
+        static public DataTable GetInternationalLicenses(int DriverID)
+        {
+            return clInternationalLicenseData.LoadInternationalDriverLicenses(DriverID);
+        }
+
+        static public clDriver FindByDriverID(int DriverID)
+        {
+            int PersonID = -1;
+            int CreatedByUserID = -1;
+            DateTime CreationDate = DateTime.MinValue;
+
+            if (clDriverData.LoadDriverByDriverID(DriverID, ref PersonID, ref CreatedByUserID, ref CreationDate))
+            {
+                return new clDriver(DriverID, PersonID, CreatedByUserID, CreationDate);
+
+            }
+
+            else return null;
+        }
+        static public clDriver FindByPersonID(int PersonID)
+        {
+            int DriverID = -1;
+            int CreatedByUserID = -1;
+            DateTime CreationDate = DateTime.MinValue;
+
+            if (clDriverData.LoadDriverByPersonID(ref DriverID, PersonID, ref CreatedByUserID, ref CreationDate))
+            {
+                return new clDriver(DriverID, PersonID, CreatedByUserID, CreationDate);
+
+            }
+
+            else return null;
+        }
+        bool _AddNewDriver()
+        {
+            this.ID =  clDriverData.AddDriver(this.PersonID, this.CreatedByUserID);
+            
+            return this.ID != -1;
+        }
+        bool _UpdateDriver()
+        {
+            return clDriverData.UpdateDriverInfo(this.ID, this.PersonID, this.CreatedByUserID);
+
+        }
+
+        public bool Save()
+        {
+            bool IsSaved = false;
+
+            switch (_Mode)
+            {
+                case eMode.Add:{
+                    if(_AddNewDriver())
+                    {
+                        IsSaved = true;
+                        _Mode = eMode.Update;
+                    }
+                    break;
+                }
+
+                case eMode.Update:
+                {
+                    if (_UpdateDriver()) IsSaved = true;
+                    break;
+                }
+            }
+            
+            return IsSaved;
+        }
+
+        
+    }
+}
---
+++ DVLD_DataAccess/Data/clDriverData.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess.Data
+{
+    static public class clDriverData
+    {
+        static public DataTable LoadDriversList()
+        {
+            string Query = @"SELECT * FROM Drivers_View";
+
+            DataTable dtDriversList = new DataTable();
+
+            using (SqlConnection Connection = new SqlConnection(DVLD_DataAccess.clSettings.ConnectionString))
+            {
+                using (SqlCommand Command = new SqlCommand(Query, Connection))
+                {
+                    Connection.Open();
+
+                    SqlDataReader Reader = Command.ExecuteReader();
+
+                    dtDriversList.Load(Reader);
+                }
+            }
+
+            return dtDriversList;
+        }
+
+        static public bool LoadDriverByDriverID(int DriverID, ref int PersonID, ref int CreatedByUserID, ref DateTime CreationDate)
+         {          
+            bool IsFound = false;
+
+            string Query = @"SELECT * FROM Driver WHERE DriverID = @DriverID";
+
+            using (SqlConnection connection = new SqlConnection(DVLD_DataAccess.clSettings.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(Query, connection))
+                {
+                    command.Parameters.AddWithValue("@DriverID", DriverID);
+
+                    connection.Open();
+
+                    using (SqlDataReader Reader = command.ExecuteReader())
+                    {
+                        if (Reader.Read())
+                        {
+                            CreationDate = (DateTime)Reader["CreatedDate"];
+                            PersonID = Convert.ToInt32(Reader["PersonID"]);
+                            CreatedByUserID = Convert.ToInt32(Reader["CreatedByUserID"]);
+
+                            IsFound = true;
+                        }
+                    }
+                }
+            }
+
+            return IsFound;
+            }
+        static public bool LoadDriverByPersonID(ref int DriverID, int PersonID, ref int CreatedByUserID, ref DateTime CreationDate)
+        {
+            bool IsFound = false;
+
+            string Query = @"SELECT * FROM Driver WHERE PersonID = @PersonID";
+
+            using (SqlConnection connection = new SqlConnection(DVLD_DataAccess.clSettings.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(Query, connection))
+                {
+                    command.Parameters.AddWithValue("@PersonID", PersonID);
+
+                    connection.Open();
+
+                    using (SqlDataReader Reader = command.ExecuteReader())
+                    {
+                        if (Reader.Read())
+                        {
+                            CreationDate = (DateTime)Reader["CreatedDate"];
+                            DriverID = Convert.ToInt32(Reader["DriverID"]);
+                            CreatedByUserID = Convert.ToInt32(Reader["CreatedByUserID"]);
+
+                            IsFound = true;
+                        }
+                    }
+                }
+            }
+
+            return IsFound;
+        }
+
+        static public int AddDriver(int PersonID, int CreatedByUserID)
+        {
+            int DriverID = -1;
+
+            string Query = @"INSERT INTO Drivers (PersonID, CreatedByUserID,CreationDate) VALUES (@PersonID,@CreatedByUserID,@CreationDate);
+                            SELECT SCOPE_IDENTITY()";
+
+            using (SqlConnection Connection = new SqlConnection(DVLD_DataAccess.clSettings.ConnectionString))
+            using (SqlCommand Command = new SqlCommand(Query, Connection))
+            {
+                Command.Parameters.AddWithValue("@PersonID", PersonID);
+                Command.Parameters.AddWithValue("@CreatedByUserID", CreatedByUserID);
+                Command.Parameters.AddWithValue("@CreationDate", DateTime.Now);
+
+                Connection.Open();
+
+                object Result = Command.ExecuteScalar();
+
+                if (Result != null) DriverID = Convert.ToInt32(Result);
+
+            }
+
+            return DriverID;
+        }
+        static public bool UpdateDriverInfo(int DriverID, int PersonID, int CreatedByUserID)
+        {
+            int RowsAffected = 0;
+
+            string Query = @"Update Drivers 
+                            Set PersonID = @PersonID, CreatedByUserID = @CreatedByUserID
+                            WHERE DriverID = @DriverID;";
+
+
+            using (SqlConnection Connection = new SqlConnection(DVLD_DataAccess.clSettings.ConnectionString))
+            using (SqlCommand Command = new SqlCommand(Query, Connection))
+            {
+                Command.Parameters.AddWithValue("@DriverID", DriverID);
+                Command.Parameters.AddWithValue("@PersonID", PersonID);
+                Command.Parameters.AddWithValue("@CreatedByUserID", CreatedByUserID);
+
+                Connection.Open();
+                RowsAffected = Command.ExecuteNonQuery();
+
+            }
+
+            return RowsAffected > 0;
+        }
+
+    }
+}
